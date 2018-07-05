68f286efd8e8f16b331c77f477a687e640b98fdf
jtt17 <38284925+jtt17@users.noreply.github.com>
2018-07-05 17:22:23 +0800

    Update func_isvalid_test.go

--- func_isvalid_test.go
+++ func_isvalid_test.go
@@ -12,28 +12,33 @@ func TestIsvalid(t *testing.T) {
 		t.Log("isvalid test1 passed ", ans)
 	}
 	if ans := isvalid("0.0.1"); ans != false {
-		t.Error("isvalid test failed ", ans)
+		t.Error("isvalid test2 failed ", ans)
 	} else {
-		t.Log("isvalid test passed ", ans)
+		t.Log("isvalid test2 passed ", ans)
 	}
 	if ans := isvalid("-0.0-0"); ans != false {
-		t.Error("isvalid test failed ", ans)
+		t.Error("isvalid test3 failed ", ans)
 	} else {
-		t.Log("isvalid test passed ", ans)
+		t.Log("isvalid test3 passed ", ans)
 	}
 	if ans := isvalid("-0."); ans != false {
-		t.Error("isvalid test failed ", ans)
+		t.Error("isvalid test4 failed ", ans)
 	} else {
-		t.Log("isvalid test passed ", ans)
+		t.Log("isvalid test4 passed ", ans)
 	}
 	if ans := isvalid("-0abc"); ans != false {
-		t.Error("isvalid test failed ", ans)
+		t.Error("isvalid test5 failed ", ans)
 	} else {
-		t.Log("isvalid test passed ", ans)
+		t.Log("isvalid test5 passed ", ans)
 	}
 	if ans := isvalid("0x3s-"); ans != false {
-		t.Error("isvalid test failed ", ans)
+		t.Error("isvalid test6 failed ", ans)
 	} else {
-		t.Log("isvalid test passed ", ans)
+		t.Log("isvalid test6 passed ", ans)
+	}
+	if ans := isvalid(""); ans != false {
+		t.Error("isvalid test7 failed ", ans)
+	} else {
+		t.Log("isvalid test7 passed ", ans)
 	}
 }
