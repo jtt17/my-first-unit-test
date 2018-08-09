59b2d37c7ee0f1718a4286acfb5157841c6dbb46
jtt17 <38284925+jtt17@users.noreply.github.com>
2018-08-09 15:51:40 +0800

    Update logs.go

--- logs.go
+++ logs.go
@@ -8,9 +8,10 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+	"strconv"
 	"strings"
-	"time"
 	"sync"
+	"time"
 )
 
 type configuration struct {
@@ -33,40 +34,24 @@ const (
 	fata
 )
 var (
-	program = filepath.Base(os.Args[0]) // program name
-	mu		 sync.Mutex
+	program  = strings.Split(filepath.Base(os.Args[0]), ".")[0] // program name
+	mu       sync.Mutex
 	conf     configuration
 	flog     *log.Logger
 	now      time.Time
 	curFile  *os.File
 	fileinfo *os.FileInfo
+	logmsg   []string
+	pid      = strconv.Itoa(os.Getpid()) + "] "
 )
-/*
 func loadConf() {
-	lastpath, _ := os.Getwd()
-	os.Chdir("/root/gopath/src/github.com/opensds/opensds/pkg/utils/logs/")
-	file, err := os.Open("conf.json")
-	defer file.Close()
-	os.Chdir(lastpath)
-	if err != nil {
-		Error("Error occur when open conf.json ", err)
-	}
-	decoder := json.NewDecoder(file)
-	err = decoder.Decode(&conf)
-	if err != nil {
-		Error("Error occur when decode conf.json ", err)
-	}
-}*/
-
-func loadConf() {
-
-	conf.Path = filepath.Join("/var/log/opensds", strings.Split(program, ".")[0])
+	conf.Path = filepath.Join("/var/log/opensds")
 	conf.Level = -1
 	conf.Ldate = true
 	conf.Lmicroseconds = true
 	conf.LogToFile = true
 	conf.LogToStdErr = true
-	conf.MaxSize = 1024*1024 *1
+	conf.MaxSize = 1024*1024 *10
 }
 
 func exits(path string) bool {
@@ -88,9 +73,16 @@ func mkdir() {
 		Fatal(e)
 	}
 }
+const flushInterval = 30 * time.Second
+func flushDaemon(){
+	for _ = range time.NewTicker(flushInterval).C {
+		outPut()
+	}
+}
 func init() {
 	loadConf()
 	mkdir()
+	go flushDaemon()
 	var tmp int = 0
 	if conf.Ldate {
 		tmp |= log.Ldate
@@ -106,31 +98,32 @@ func init() {
 	}
 	log.SetFlags(tmp)
 	curFile, fileinfo = initFile(conf.Path)
+	logmsg = []string{}
 }
 
 func initFile(path string) (*os.File, *os.FileInfo) {
 	files, err := ioutil.ReadDir(path)
 	if err != nil {
-		Error(err)
 		return nil, nil
 	}
 	l := len(files)
-	fmt.Println("Got Log File List :",files)
 	if l == 0 {
 		return nil, nil
 	}
-	if uint64(files[l-1].Size()) >= conf.MaxSize {
+	var tmpinfo os.FileInfo
+	for i := l-1 ; i >= 0 ; i -- {
+		if strings.Contains(files[i].Name(), program) && uint64(files[i].Size()) < conf.MaxSize {
+			tmpinfo = files[i]
+			break
+		}
+	}
+	if tmpinfo == nil {
 		return nil, nil
 	}
-	tmpfile, e1 := os.OpenFile(filepath.Join(conf.Path, files[l-1].Name()), os.O_WRONLY|os.O_APPEND, 0666)
+	tmpfile, e1 := os.OpenFile(filepath.Join(conf.Path, tmpinfo.Name()), os.O_WRONLY|os.O_APPEND, 0666)
 	if e1 != nil {
 		return nil, nil
 	}
-	tmpinfo, e2 := os.Stat(tmpfile.Name())
-	if e2 != nil {
-		return nil,nil
-	}
-	fmt.Println("INIT: Now should USE FILE: ",tmpfile.Name())
 	return tmpfile, &tmpinfo
 }
 
@@ -138,20 +131,15 @@ func open()(*os.File,*os.FileInfo) {
 	if fileinfo == nil {
 		return nil,nil
 	}
-	fmt.Println("Attempt to OPEN file ",(*fileinfo).Name())
 	file, err := os.OpenFile(filepath.Join(conf.Path, (*fileinfo).Name()), os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		fmt.Println("OPEN: Can not open Log file",(*fileinfo).Name())
-		Error("can not open log file :", err)
 		return nil, nil
 	}
-	fmt.Println("OPEN : open file success ",file.Name())
-//    file.WriteString(fmt.Sprintf("Log file Append at: %v\n\n", time.Now().Format("2018-01-02 15:04:05.000000")))
 	tmpinfo, _ := os.Stat(file.Name())
 	return file, &tmpinfo
 }
 func create()(*os.File, *os.FileInfo)  {
-	name := fmt.Sprintf("%s %04d-%02d-%02d %02d.%02d.%02d.log",
+	name := fmt.Sprintf("%s_%04d-%02d-%02d_%02d.%02d.%02d.log",
 		filepath.Join(conf.Path, program),
 		time.Now().Year(),
 		time.Now().Month(),
@@ -159,13 +147,20 @@ func create()(*os.File, *os.FileInfo)  {
 		time.Now().Hour(),
 		time.Now().Minute(),
 		time.Now().Second())
-	fmt.Println("CREATE : now create file ",name )
 	file, err := os.OpenFile(name, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
-		fmt.Printf("create file failed %v\n", err)
 		return nil, nil
 	}
-	file.WriteString(fmt.Sprintf("Log file Create at: %v\n\n", time.Now().Format("2018-01-02 15:04:05.000000")))
+	file.WriteString(fmt.Sprintf("Log file Create at: %04d-%02d-%02d %02d:%02d:%02d:%06d\n\n",
+		time.Now().Year(),
+		time.Now().Month(),
+		time.Now().Day(),
+		time.Now().Hour(),
+		time.Now().Minute(),
+		time.Now().Second(),
+		time.Now().Nanosecond()/1000))
+
+	file.WriteString(fmt.Sprintf("Binary: Built with %s %s for %s %s\n\n", runtime.Compiler, runtime.Version(), runtime.GOOS, runtime.GOARCH))
 	tmpinfo ,e := os.Stat(name)
 	if e != nil {
 		fmt.Println("get fileinfo failed ",e)
@@ -176,134 +171,137 @@ func create()(*os.File, *os.FileInfo)  {
 
 //
 //
-func doPrint(s string) {
+func outPut() {
 	mu.Lock()
 	defer mu.Unlock()
 	curFile, fileinfo = open()
 	if fileinfo == nil || (uint64)((*fileinfo).Size()) >= conf.MaxSize {
-		fmt.Println("DOPRINT: but still need create ")
 		curFile, fileinfo = create()
 	}
 	flog = log.New(curFile, "", log.Flags())
-	flog.Println(s)
+	for _, x := range logmsg {
+		flog.Println(x)
+	}
 	curFile.Close()
+	logmsg = []string{}
+}
+func doPrint(s string) {
+	mu.Lock()
+	logmsg = append(logmsg, s)
+	mu.Unlock()
 }
 func doInfo(v string) {
 	if info < conf.Level {
 		return
 	}
+	_, file, link, _ := runtime.Caller(2)
+	s1 := fmt.Sprint("[INFO]: ", file[strings.LastIndex(file,"opensds"):], " ", link, " [PID:", pid)
 	if conf.LogToStdErr {
-		log.Println(v)
+		log.Println(s1 + v)
 	}
 	if conf.LogToFile {
-		doPrint(v)
+		doPrint(s1 + v)
 	}
 }
 func Info(v ...interface{}) {
-	_, file, link, _ := runtime.Caller(1)
-	s := fmt.Sprint("[Info]: ", file, " ", link, v)
+	s := fmt.Sprint(v)
 	doInfo(s)
 }
 func Infof(format string, v ...interface{}) {
-	_, file, link, _ := runtime.Caller(1)
-	s := fmt.Sprint("[Info]: ", file, " ", link, v)
-	doInfo(s)
+	s := fmt.Sprintf(format, v...)
+	doInfo("[" + s + "]")
 }
 func Infoln(v ...interface{}) {
-	_, file, link, _ := runtime.Caller(1)
-	s := fmt.Sprintln("[Info]:", file, link, v)
+	s := fmt.Sprintln(v)
 	doInfo(s)
 }
 func doWarn(v string) {
 	if warn < conf.Level {
 		return
 	}
+	_, file, link, _ := runtime.Caller(2)
+	s1 := fmt.Sprint("[WARN]: ", file[strings.LastIndex(file,"opensds"):], " ", link, " [PID:", pid)
 	if conf.LogToStdErr {
-		log.Println(v)
+		log.Println(s1 + v)
 	}
 	if conf.LogToFile {
-		doPrint(v)
+		doPrint(s1 + v)
 	}
 }
-
 func Warning(v ...interface{}) {
-	_, file, link, _ := runtime.Caller(1)
-	s := fmt.Sprint("[Warn]: ", file, " ", link, v)
+	s := fmt.Sprint(v)
 	doWarn(s)
 }
 func Warningf(format string, v ...interface{}) {
-	_, file, link, _ := runtime.Caller(1)
-	s := fmt.Sprint("[Warn]: ", file, " ", link, v)
-	doWarn(s)
+	s := fmt.Sprintf(format, v...)
+	doWarn("[" + s + "]")
 }
 func Warningln(v ...interface{}) {
-	_, file, link, _ := runtime.Caller(1)
-	s := fmt.Sprintln("[Warn]:", file, link, v)
+	s := fmt.Sprintln(v)
 	doWarn(s)
 }
 func doError(s string) {
 	if erro < conf.Level {
 		return
 	}
+	_, file, link, _ := runtime.Caller(2)
+	s1 := fmt.Sprint("[ERRO]: ", file[strings.LastIndex(file,"opensds"):], " ", link, " [PID:", pid)
 	if conf.LogToStdErr {
-		log.Println(s)
+		log.Println(s1 + s)
 	}
 	if conf.LogToFile {
-		doPrint(s)
+		doPrint(s1 + s)
 	}
 }
-
 func Error(v ...interface{}) {
-	_, file, link, _ := runtime.Caller(1)
-	s := fmt.Sprint("[Erro]: ", file, " ", link, v)
+	s := fmt.Sprint(v)
 	doError(s)
 }
 func Errorf(format string, v ...interface{}) {
-	_, file, link, _ := runtime.Caller(1)
-	s := fmt.Sprint("[Erro]: ", file, " ", link, v)
-	doError(s)
+	s := fmt.Sprintf(format, v...)
+	doError("[" + s + "]")
 }
 func Errorln(v ...interface{}) {
-	_, file, link, _ := runtime.Caller(1)
-	s := fmt.Sprintln("[Erro]:", file, link, v)
+	s := fmt.Sprintln(v)
 	doError(s)
 }
 func doFatal(s string) {
 	if fata < conf.Level {
 		return
 	}
+	_, file, link, _ := runtime.Caller(2)
+	s1 := fmt.Sprint("[FATA]: ", file[strings.LastIndex(file,"opensds"):], " ", link, " [PID:", pid)
 	if conf.LogToStdErr {
-		log.Println(s)
+		log.Println(s1 + s)
 	}
 	if conf.LogToFile {
-		doPrint(s)
+		doPrint(s1 + s)
 	}
 	if curFile != nil {
 		curFile.Close()
 	}
-	os.Exit(1)
+	//	FlushLogs()
+	//	os.Exit(1)
 }
 func Fatal(v ...interface{}) {
-	_, file, link, _ := runtime.Caller(1)
-	s := fmt.Sprint("[Fata]: ", file, " ", link, v)
+	s := fmt.Sprint(v)
 	doFatal(s)
 }
 func Fatalf(format string, v ...interface{}) {
-	_, file, link, _ := runtime.Caller(1)
-	s := fmt.Sprint("[Fata]: ", file, " ", link, v)
-	doFatal(s)
+	s := fmt.Sprintf(format, v...)
+	doFatal("[" + s + "]")
 }
 func Fatalln(v ...interface{}) {
-	_, file, link, _ := runtime.Caller(1)
-	s := fmt.Sprintln("[Fata]:", file, link, v)
+	s := fmt.Sprintln(v)
 	doFatal(s)
 }
 
 func FlushLogs() {
+	outPut()
+}
+func InitLogs() {
+}
 
-}
-func InitLogs(){
-}
 type Verbose bool
 
 func V(level int32) Verbose {
@@ -314,23 +312,19 @@ func V(level int32) Verbose {
 }
 func (v Verbose) Info(args ...interface{}) {
 	if v {
-		_, file, link, _ := runtime.Caller(1)
-		s := fmt.Sprint("[Info]: ", file, " ", link, args)
+		s := fmt.Sprint(args)
 		doInfo(s)
 	}
 }
 func (v Verbose) Infof(format string, args ...interface{}) {
 	if v {
-		_, file, link, _ := runtime.Caller(1)
-		s := fmt.Sprint("[Info]: ", file, " ", link, args)
-		doInfo(s)
+		s := fmt.Sprintf(format, args...)
+		doInfo("[" + s + "]")
 	}
 }
 func (v Verbose) Infoln(args ...interface{}) {
 	if v {
-		_, file, link, _ := runtime.Caller(1)
-		s := fmt.Sprintln("[Info]: ", file, " ", link, args)
+		s := fmt.Sprintln(args)
 		doInfo(s)
 	}
 }
-
